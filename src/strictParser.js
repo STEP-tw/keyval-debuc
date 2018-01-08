b9c0ff0f38f9cd4cc8059f4b639d55a529434cca
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2018-01-08 18:27:45 +0530

    Strict parser has default true case sensitivity

--- src/strictParser.js
+++ src/strictParser.js
@@ -1,7 +1,7 @@
 const Parser=require("./keyValueParser.js");
 const strictParseInfoCreator=require("./parseInfoCreator.js").strict;
 
-var StrictParser=function(listOfKeys,caseSensitive) {
+var StrictParser=function(listOfKeys,caseSensitive=true) {
   Parser.call(this);
   let sanitisedListOfKeys=listOfKeys||[];
   this.parseInfoCreator=strictParseInfoCreator(sanitisedListOfKeys,caseSensitive);
