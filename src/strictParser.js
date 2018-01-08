fbb9919c3ba0a1bfd5305d0a99045a0f0a54e72a
Debarun Chakraborty <debarun.chakraborty96@gmail.com>
2018-01-08 17:49:14 +0530

    All node's assert tests from testStrictParser.js are replaced with chai's assert

--- src/strictParser.js
+++ src/strictParser.js
@@ -1,7 +1,7 @@
 const Parser=require("./keyValueParser.js");
 const strictParseInfoCreator=require("./parseInfoCreator.js").strict;
 
-var StrictParser=function(listOfKeys,) {
+var StrictParser=function(listOfKeys,caseSensitive) {
   Parser.call(this);
   let sanitisedListOfKeys=listOfKeys||[];
   this.parseInfoCreator=strictParseInfoCreator(sanitisedListOfKeys);
